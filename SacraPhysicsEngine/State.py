3077869fa8ff61c551381039da818e3af24620b6
Andreas Evensen <58504549+thesombady@users.noreply.github.com>
2020-08-02 21:32:55 +0200

    begun _Collision method

--- SacraPhysicsEngine/State.py
+++ SacraPhysicsEngine/State.py
@@ -1,4 +1,5 @@
 import sacramathengine
+from math import sin, cos, tan, pi
 
 class StateError(Exceptions):
     pass
@@ -36,25 +37,61 @@ class State:
                     self.ActiveMeshes.append(self.Mesh)
                 else:
                     raise StateError("[System]: State-Objects need MeshObjects as input in the _setter function.")
+            try:
+                self._CenterOfMass()
+            except:
+                raise StateError("[System]: Cannot compute center of mass, fault is in _setter method.")
 
 
     def _CenterOfMass(self):
+        """A function that is called when using _setter method; Provides the center of mass of that mesh aswell as computing the largest offset."""
         if  isinstance(self.Mesh, (list, tuple)):
             try:
                 Colletion = []
                 for tri in self.Mesh:
                     CenterOfTriangle = 1/3 * (tri[0] + tri[1] + tri[2])
                     Collection.append(CenterOfTriangle)
-                CenterOfMass = len(Collection) *
-                pass
+                CenterOfMass = vec3d(0, 0, 0)
+                for i in range(len(Collection)):
+                    CenterOfMass += Collection[i]
+                self.CenterOfMass = CenterOfMass / len(Collection)
+                try:
+                    def Maximum(*args):
+                        for i in range(len(args)):
+                            if not isinstance(args[i], Triangle):
+                                raise TypeError("[System]: The input in Maximum function is not of correct format.")
+                        MaxNorms = []
+                        for tri in args:
+                            vec1 = (self.CenterOfMass - tri[0]).norm()
+                            vec2 = (self.CenterOfMass - tri[1]).norm()
+                            vec3 = (self.CenterOfMass - tri[2]).norm()
+                            NormsOfTri_i = [vec1, vec2, vec3]
+                            MaxNorms.append(max(NormsOfTri_i))
+                        self.MaxNorm = max(MaxNorms)
+
+                    #compute the largest vector, then apply a rotation to that vector so it covers 360 degress around
+                except:
+                    raise StateError("[System]: Can't compute the radius")
             except:
-                raise StateError("[System]: Cannot compute center of mass.")
+                raise StateError("[System]: Can not compute center of mass.")
         else:
             raise TypeError("[System]: Can not load the center of mass, due to Type-Error of the mesh."):
 
 
+
     def _Collision(self, Other):
-        pass
+        try:
+            if not isinstance(Other, state) and Other.state != "NonSolid":
+                Epsilon = self.CenterOfMass - Other.CenterOfMass
+                Delta = self.MaxNorm - Other.MaxNorm
+                #Need to add __lq__ to vectors...
+                if delta < 0 and ...:
+                pass
+            else:
+                raise StateError("[System]: Can not compute _Collision method.")
+        except:
+            raise StateError("[System]: Can not compute _Collision method.")
+
 
     def __str__(self):
         return f'{self.state}'
