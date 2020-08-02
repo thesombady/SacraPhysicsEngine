1d72f00c727cdee795fc17dcd1235d311d28e6fc
Andreas Evensen <58504549+thesombady@users.noreply.github.com>
2020-08-02 21:08:12 +0200

    Update State.py

--- SacraPhysicsEngine/State.py
+++ SacraPhysicsEngine/State.py
@@ -29,23 +29,35 @@ class State:
             except:
                 raise StateError("[System]: Cannot load a state for the object.")
             if Mesh == None:
-                raise StateError("[System]: Each State-Object needs a mesh; Refer to SacraMathEngine.Mesh")
+                raise StateError("[System]: Each State-Object needs a mesh; Refer to SacraMathEngine.Mesh .")
             else:
                 if isinstance(Mesh, MeshObject): #Might need to rework that line
-                    self.Mesh = Mesh
+                    self.Mesh = Mesh.Mesh
                     self.ActiveMeshes.append(self.Mesh)
                 else:
-                    raise StateError("[System]: State-Objects need MeshObjects as input in the _setter function")
+                    raise StateError("[System]: State-Objects need MeshObjects as input in the _setter function.")
 
 
     def _CenterOfMass(self):
-        pass
+        if  isinstance(self.Mesh, (list, tuple)):
+            try:
+                Colletion = []
+                for tri in self.Mesh:
+                    CenterOfTriangle = 1/3 * (tri[0] + tri[1] + tri[2])
+                    Collection.append(CenterOfTriangle)
+                CenterOfMass = len(Collection) *
+                pass
+            except:
+                raise StateError("[System]: Cannot compute center of mass.")
+        else:
+            raise TypeError("[System]: Can not load the center of mass, due to Type-Error of the mesh."):
+
 
     def _Collision(self, Other):
         pass
 
     def __str__(self):
-        return f'{self.states}'
+        return f'{self.state}'
 
     def __repr__(self):
-        return f'{self.states}'
+        return f'{self.state}'
