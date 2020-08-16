2a150e1fd6e444e312bb00ba9d1f79b92a581d8f
Andreas Evensen <58504549+thesombady@users.noreply.github.com>
2020-08-16 14:23:20 +0200

    Update Movement.py

---
+++ SacraPhysicsEngine/Movement.py
@@ -0,0 +1,15 @@
+from sacramathengine import *
+from State import State, StateError
+
+class Movement:
+
+
+    def __init__(self):
+        pass
+
+    def _setter(self, MeshObject):
+        self.Mesh = MeshObject.Mesh()
+
+    def _Move(self, Vector):
+
+        pass
