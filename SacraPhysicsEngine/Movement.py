b43be8c03249aa30f9eb74426769dae3895f6fea
Andreas Evensen <andreas.evensen11@gmail.com>
2020-08-30 12:36:18 +0200

    Update Movement.py

--- SacraPhysicsEngine/Movement.py
+++ SacraPhysicsEngine/Movement.py
@@ -4,12 +4,15 @@ from State import State, StateError
 class Movement:
 
 
-    def __init__(self):
-        pass
+    def __init__(self, Vector, MeshObject = None):
+        if not isinstance(Vector, (vec3d, vec4d)):
+            raise TypeError("[System]: Vector has to be of type vec3d, or vec4d.")
+        self.Vector = Vector
+        if MeshObject == None:
+            print("[System]: Call the _setter method to add a MeshObject to the class.")
 
     def _setter(self, MeshObject):
         self.Mesh = MeshObject.Mesh()
 
     def _Move(self, Vector):
-
-        pass
+        return Movement(self.Vector, MeshObject)
