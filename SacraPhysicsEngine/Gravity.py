a81634dba6ff7780af979fb20ff3331b8693fbf5
Andreas Evensen <andreas.evensen11@gmail.com>
2020-08-30 12:52:04 +0200

    gravity fixed

--- SacraPhysicsEngine/Gravity.py
+++ SacraPhysicsEngine/Gravity.py
@@ -1,9 +1,17 @@
 from .State import State
+from SacraMathEngine import * #Do not use this in final version. Need it right now for the mesh, and vector definitions.
+from .State import State, StateError
 
 global FPS
 
+class GravityIssue(Exception):
+    pass
+
 class Gravity:
     GravityPull = 9.82
+
+    GravityVector = vec3d(0, -self.GravityPull, 0)
+
     def __init__(self):
         pass
 
@@ -13,10 +21,18 @@ class Gravity:
         elif is not isinstance(Object, State):
             raise TypeError("[System]: Gravity must have an State-object")
         else:
-            self.CenterOfMass =
+            try:
+                self.Object = Object
+                self.MeshObject = Object.Mesh
+            except:
+                raise TypeError("[System]: Cannot give attribute mesh from the state.")
+        try:
+            _Update()
+        except:
+            pass
 
-    def __str__(self):
-        return f'{None}'
-
-
-Gravity(60)
+    def _Update(self):
+        NewMesh = self.MeshObject + self.GravityVector
+        State = self.Object.state
+        NewObject = State()
+        return NewObject._setter(State, NewMesh)
