c5ac2ea7f56e82cce7c839c30339f6347b08a18c
Andreas Evensen <andreas.evensen11@gmail.com>
2020-09-02 16:58:10 +0200

    pypi upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SacraPhysicsEngine
- The Physics Engine for Sacra Game Engine
+ The Physics Engine for Sacra Game Engine. This is developed by Andreas Evensen. This is developed as a project for the game engine "Sacra" and is ment to be used for that only, however if it meet's your requirements then feel free to use it; Since it's under the MIT License
+# Installation
+Using SacraPhysicsEngine is easy, all you need is to pip-install, the packageing will import everything that is nessicary for you. It requires a few number of packages, such as SacraMathEngine, random and math.
+```bash
+pip install SacraPhysicsEngine
+```
+# Usage
+As stated prior, this is ment for the game engine Sacra, however if it suits your needs feel free to use it. It is still under development, but it's is an open-source project, which can be found on github.
--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("readme.md", "r") as readme_file:
+    long_description = readme_file.read()
+
 
 setup(
     name = "SacraPhysicsEngine",
+    version = "0.0.1",
     author = "Andreas Evensen",
-    version = "0.0.1"
+    author_email = "Andreas.evensen11@gmail.com",
+    description = "A lightweight physics-engine package with intents of being used in Sacra Game Engine",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    py_modules = ["Gravity", "State", "Movement"],
+    #package_dir = {'':'src'},
+    include_package_data = True,
+    packages = find_packages(),
+    license = "MIT",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License"
+    ],
 )
